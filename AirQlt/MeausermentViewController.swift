fbd2ffd4527b0cc0e0ca8cfe0ff1ab5a24ae17b5
Dariusz Kulig <darek@MacBook-Pro-Dariusz.local>
2017-03-10 18:11:16 +0100

    fixed issues with navigation controller

--- AirQlt/MeausermentViewController.swift
+++ AirQlt/MeausermentViewController.swift
@@ -14,8 +14,7 @@ class MesauermentViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName:UIColor.white]
     }
     
     override func didReceiveMemoryWarning() {
