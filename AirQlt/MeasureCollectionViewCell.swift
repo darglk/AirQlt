857b963f2385d1b90e9de98f70acc614efef206b
Dariusz Kulig <darek@MacBook-Pro-Dariusz.local>
2017-03-10 20:17:09 +0100

    added outlets to created views in swift files

---
+++ AirQlt/MeasureCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MeasureCollectionViewCell.swift
+//  AirQlt
+//
+//  Created by Dariusz Kulig on 10.03.2017.
+//  Copyright © 2017 Dariusz Kulig. All rights reserved.
+//
+
+import UIKit
+
+class MeasureCollectionViewCell: UICollectionViewCell {
+    
+}
