edc1ea060fbf0cecb1b5cf1fa22a8013161d966b
Dariusz Kulig <darek@MacBook-Pro-Dariusz.local>
2017-03-10 15:11:58 +0100

    Initial Commit

---
+++ AirQlt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AirQlt
+//
+//  Created by Dariusz Kulig on 10.03.2017.
+//  Copyright © 2017 Dariusz Kulig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
